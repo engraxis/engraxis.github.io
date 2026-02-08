10e430f542fef31a976bf054134788888938d840
Abdullah Tahir <47785428+engraxis@users.noreply.github.com>
2026-02-08 19:09:38 +0100

    Update social-icons.js

--- assets/js/social-icons.js
+++ assets/js/social-icons.js
@@ -4,7 +4,7 @@ const socialLinks = [
     { name: "LinkedIn", url: "https://www.linkedin.com/in/engrabxis/", iconClass: "fab fa-linkedin" },
     { name: "GitHub", url: "https://github.com/engraxis", iconClass: "fab fa-github" },
     { name: "Google Scholar", url: "https://scholar.google.com/citations?hl=en&user=y4iZqrgAAAAJ&view_op=list_works&sortby=pubdate", iconImg: "assets/images/scholar_icon.png" },
-    { name: "Research Gate", url: "https://scholar.google.com/citations?hl=en&user=y4iZqrgAAAAJ&view_op=list_works&sortby=pubdate", iconImg: "assets/images/r_gate_icon.png" },
+    { name: "Research Gate", url: "https://www.researchgate.net/profile/Abdullah-Tahir-3?ev=hdr_xprf", iconImg: "assets/images/r_gate_icon.png" },
     { name: "Website", url: "https://vbn.aau.dk/da/persons/atah/", iconClass: "fas fa-globe" },
     { name: "Facebook", url: "https://www.facebook.com/dead.abx/", iconClass: "fab fa-facebook" },
 ];
